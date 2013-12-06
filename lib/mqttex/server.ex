dc50eecc96d1d7ee53d872ad714f06635c6dd294
Klaus Alfert <klaus.alfert@googlemail.com>
2013-12-06 22:36:59 +0100

    Implemented Reconnection

--- lib/mqttex/server.ex
+++ lib/mqttex/server.ex
@@ -16,7 +16,27 @@ defmodule Mqttex.Server do
 	defrecord ConnectionState, connection: :none, client_proc: :none, subscriptions: []
 
 	#### API of the server
+	@doc """
+	Connects the client with the server process. If no server for the `client_id` exists, 
+	then a new server is started and linked to the supervisor tree. If the server already 
+	exists, we have a reconnection situation: if the server is in state `disconnected` 
+	the reconnection can be executed. Otherwise an error occurs.
+	"""
+	@spec connect(Mqttex.Connection.t, pid) :: Mqttex.ConnAckMsg.t | {Mqttex.ConnAckMsg.t, pid}
+	def connect(Mqttex.Connection[] = connection, client_proc // self()) do
+		case :global.whereis_name(connection.client_id) do
+			:undefined -> start_link(connection, client_proc)
+			pid -> reconnect(pid, connection, client_proc)
+		end
+	end
 
+	@doc "Reconnects an existing server with a new connection"	
+	defp reconnect(server, connection, client_proc) do
+		case :gen_fsm.sync_send_event(server, {:reconnect, connection, client_proc}) do
+			{:ok, pid}       -> {Mqttex.ConnAckMsg.new([status: :ok]), pid}
+			{:error, reason} -> Mqttex.ConnAckMsg.new [status: reason]
+		end
+	end
 	
 	@spec start_link(Mqttex.Connection.t, pid) :: Mqttex.ConnAckMsg.t | {Mqttex.ConnAckMsg.t, pid}
 	def start_link( Mqttex.Connection[] = connection, client_proc // self()) do
@@ -33,11 +53,17 @@ defmodule Mqttex.Server do
 		Mqttex.PingRespMsg.new
 	end
 	
-	@spec disconnect(pid, Mqttex.DisconnectMsg.t) :: :none
+	@spec disconnect(pid, Mqttex.DisconnectMsg.t) :: :ok
 	def disconnect(server, _diconnect_msg) do
 		:gen_fsm.send_event(server, :disconnect)
 	end
 
+	@doc "Internal function for stopping the server"
+	@spec stop(pid) :: :ok
+	def stop(server) do
+		:gen_fsm.send_all_state_event(server, :stop)
+	end
+	
 
 	#### Internal functions
 
@@ -52,7 +78,7 @@ defmodule Mqttex.Server do
 	end
 
 	@doc "Events in state `clean_session` with replies"
-	def clean_session(:ping, from, ConnectionState[]=state) do
+	def clean_session(:ping, _from, ConnectionState[]=state) do
 		{:reply, :pong, :clean_session, state, state.connection.keep_alive_server}
 	end
 
@@ -65,6 +91,34 @@ defmodule Mqttex.Server do
 	def clean_session(:disconnect, ConnectionState[]=state) do
 		# TODO: call unsubscribe all topics
 		{:next_state, :clean_disconnect, state}
+	end
+	
+	# Reconnection with reply
+	def clean_disconnect({:reconnect, connection, client_proc}, _from, ConnectionState[]=state) do
+		{reply, s, new_state_data} = case check_connection(connection) do
+			:ok -> {{:ok, self}, :clean_session, 
+				state.update [connection: connection, client_proc: client_proc]}
+			error -> {{:error, error}, :clean_disconnect, state}
+		end
+		{:reply, reply, s, new_state_data, new_state_data.connection.keep_alive_server}
+	end
+
+	@doc "The stop event is only allowed if "
+	def handle_event(:stop, _state, ConnectionState[subscriptions: []] = state_data) do
+		{:stop, :normal, state_data}
+	end
+	def handle_event(:stop, state, ConnectionState[] = state_data) do
+		{:next_state, state, state_data}
+	end
+		
+
+	@doc "Checks the connection"
+	@spec check_connection(Mqttex.Connection.t) :: Mqttex.conn_ack_type
+	def check_connection(Mqttex.Connection[client_id: c_id]) 
+		when length(c_id)> 26, do: :identifier_rejected
+	def check_connection(Mqttex.Connection[] = _connection) do
+		# TODO: check authentication
+		:ok
 	end
 	
 
--- test/mqttex_test.exs
+++ test/mqttex_test.exs
@@ -7,10 +7,29 @@ defmodule MqttexTest do
 
 	test "start server and ping it" do
 		connection = Mqttex.Connection.new [client_id: "MqttexTest"]
-		{Mqttex.ConnAckMsg[], server} = Mqttex.Server.start_link(connection, self)
+		{Mqttex.ConnAckMsg[], server} = Mqttex.Server.connect(connection, self)
 		assert(is_pid(server))
 
 		ping = Mqttex.PingReqMsg.new
-		Mqttex.PingRespMsg[] = Mqttex.Server.ping(server, ping)
+		assert Mqttex.Server.ping(server, ping) == Mqttex.PingRespMsg[]
+
+		assert Mqttex.Server.stop(server) == :ok
 	end
+
+	test "start server and reconnect" do
+		connection = Mqttex.Connection.new [client_id: "MqttexTest"]
+		{Mqttex.ConnAckMsg[], server} = Mqttex.Server.connect(connection, self)
+		assert(is_pid(server))
+
+		dis = Mqttex.DisconnectMsg.new
+		assert Mqttex.Server.disconnect(server, dis) == :ok
+
+		{ack, s} = Mqttex.Server.connect(connection, self)
+		assert ack == Mqttex.ConnAckMsg.new
+		assert s == server
+
+		assert Mqttex.Server.stop(server) == :ok
+	end
+
+
 end
