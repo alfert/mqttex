ca8558995f651c4810d9fe536e213c6ca6b98cd1
Klaus Alfert <klaus.alfert@googlemail.com>
2013-12-15 21:56:41 +0100

    Publish, subscribe and unsubscribe for :fire_and_forget

--- lib/mqttex/topic.ex
+++ lib/mqttex/topic.ex
@@ -10,10 +10,30 @@ defmodule Mqttex.Topic do
 		:gen_server.start_link(topic)
 	end
 	
-	def publish(msg, from) do
-		:gen_server.call(topic_server(msg), {:publish, msg, from})
+	def publish(Mqttex.PublishMsg[] msg, client) do
+		:gen_server.call(topic_server(msg), {:publish, msg, client})
 	end
 	
+	@doc """
+	Subscribes a `client` to the `topic` with requested `qos`. 
+	Returns the granted qos.
+	"""
+	@spec subscribe(binary, qos_type, binary) :: qos_type
+	def subscribe(topic, qos, client) do
+		:gen_server.call(topic_server(topic), {:subscribe, client, qos})
+	end
+
+	@doc """
+	Unsubscribes a `client` from the `topic`. It is guaranteed that 
+	the client is no longer subscribed to the topic.
+	"""
+	@spec unsubscribe(binary, binary) :: :ok
+	def unsubscribe(topic, client) do
+		:gen_server.call(topic_server(topic), {:unsubscribe, client})
+	end
+		
+
+	@doc "Returns the name of the `TopicServer` for the given topic."
 	def topic_server(Mqttex.PublishMsg[topic: topic]) do
 		topic_server(topic)
 	end
@@ -25,5 +45,40 @@ defmodule Mqttex.Topic do
 		{:ok, State.new[topic: topic]}
 	end
 	
+
+	####################################################################################
+	### Callbacks
+	####################################################################################
+
+	def handle_call({:publish, Mqttex.PublishMsg[message: content] _msg, client}, 
+				    from, State[subscriptions: subs] = _state) do
+		Enum.each(subs, fn({session, qos}) -> send_msg(session, content, qos) end)
+		{:reply, :ok, state}
+	end
+	def handle_call({:subscribe, client, qos}, _from, State[subscriptions: subs] = state) do
+		new_state = state.subscriptions [{client, qos} | subs]
+		{:reply, :fire_and_forget, new_state}
+	end
+	def handle_call({:unsubscribe, client}, _from, State[subscriptions: subs] = state) do
+		new_subs = Enum.filter(subs, 
+				fn {client, _} -> false 
+					_          -> true end)
+		new_state = state.subscriptions new_subs
+		{:reply, :ok, new_state}
+	end
+
+	####################################################################################
+	### Internal Implementation
+	####################################################################################
+
+	def send_msg(session, content, :fire_and_forget) do
+		Mqttex.Server.send_msg(session, content, :fire_and_forget)
+	end
+	
+
+	def handle_call({:unsubscribe, client, qos}, _from, State[subscriptions: subs] = state) do
+		new_state = state.subscriptions [{client, qos} | subs]
+		{:reply, :fire_and_forget, new_state}
+	end
 
 end
