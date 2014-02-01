c4c53012a0943de1329833cef2d79907c377d384
Klaus Alfert <klaus.alfert@googlemail.com>
2014-02-01 18:09:26 +0100

    Many tests no longer commented out.

--- test/mqttex_queue_test.exs
+++ test/mqttex_queue_test.exs
@@ -6,96 +6,105 @@ defmodule MqttexQueueTest do
 
 	use ExUnit.Case
 
-	# test "Simple Send via Queue" do
-	# 	{q, _qIn} = setupQueue()
-	# 	Mqttex.Test.SessionAdapter.send_msg(q, :hallo)
-	# 	assert_receive :hallo, 200
-	# end
+	test "Simple Send via Queue" do
+		{q, qIn} = setupQueue()
+		ref = Process.monitor(qIn)
+		IO.puts "q is #{inspect q}"
+		IO.puts "qIn is #{inspect qIn}"
+		Mqttex.Test.SessionAdapter.send_msg(q, :hello)
+		# this must fail, hello is not a proper message 
+		assert_receive :hello, 200
+		# receive do
+		# 	msg -> IO.puts "Got message #{inspect msg}"
+		# 	after 200 -> IO.puts "timeout :-("
+		# end
+		# assert_receive {:DONE, ^ref, _, _, _},  200
+	end
 
-	# test "Publish FaF via Queue" do
-	# 	{q, _qIn} = setupQueue()
-	# 	msg = "Initial Message FaF"
-	# 	Mqttex.Test.SessionAdapter.publish(q, "FAF-Topic", msg, :fire_and_forget)
+	test "Publish FaF via Queue" do
+		{q, _qIn} = setupQueue()
+		msg = "Initial Message FaF"
+		Mqttex.Test.SessionAdapter.publish(q, "FAF-Topic", msg, :fire_and_forget)
 
-	# 	assert_receive Mqttex.PublishMsg[message: ^msg], 1_000
-	# end
+		assert_receive Mqttex.PublishMsg[message: ^msg], 1_000
+	end
 
-	# test "Publish ALO via Queue" do
-	# 	{q, _qIn} = setupQueue()
-	# 	msg = "Initial Message ALO"
-	# 	Mqttex.Test.SessionAdapter.publish(q, "ALO-Topic", msg, :at_least_once)
+	test "Publish ALO via Queue" do
+		{q, _qIn} = setupQueue()
+		msg = "Initial Message ALO"
+		Mqttex.Test.SessionAdapter.publish(q, "ALO-Topic", msg, :at_least_once)
 
-	# 	assert_receive Mqttex.PublishMsg[message: ^msg], 1_000
-	# end
+		assert_receive Mqttex.PublishMsg[message: ^msg], 1_000
+	end
 
-	# test "Publish AMO via Queue" do
-	# 	{q, qIn} = setupQueue()
-	# 	msg = "Initial Message AMO"
-	# 	Mqttex.Test.SessionAdapter.publish(q, "AMO-Topic", msg, :at_most_once)
+	test "Publish AMO via Queue" do
+		{q, qIn} = setupQueue()
+		msg = "Initial Message AMO"
+		Mqttex.Test.SessionAdapter.publish(q, "AMO-Topic", msg, :at_most_once)
 
-	# 	assert_receive Mqttex.PublishMsg[message: ^msg] = any, 1_000
-	# end
+		assert_receive Mqttex.PublishMsg[message: ^msg] = any, 1_000
+	end
 
-	# test "Publish ALO via Lossy Queue" do
-	# 	{q, qIn} = setupQueue(70)
-	# 	msg = "Lossy Message ALO"
-	# 	Mqttex.Test.SessionAdapter.publish(q, "ALO-Topic", msg, :at_least_once)
+	test "Publish ALO via Lossy Queue" do
+		{q, qIn} = setupQueue(70)
+		msg = "Lossy Message ALO"
+		Mqttex.Test.SessionAdapter.publish(q, "ALO-Topic", msg, :at_least_once)
 
-	# 	assert_receive Mqttex.PublishMsg[message: ^msg] = any, 1_000
-	# end
+		assert_receive Mqttex.PublishMsg[message: ^msg] = any, 1_000
+	end
 
-	# test "Publish AMO via Lossy Queue" do
-	# 	{q, qIn} = setupQueue(70)
-	# 	msg = "Lossy Message AMO"
-	# 	Mqttex.Test.SessionAdapter.publish(q, "AMO-Topic", msg, :at_most_once)
+	test "Publish AMO via Lossy Queue" do
+		{q, qIn} = setupQueue(70)
+		msg = "Lossy Message AMO"
+		Mqttex.Test.SessionAdapter.publish(q, "AMO-Topic", msg, :at_most_once)
 
-	# 	assert_receive Mqttex.PublishMsg[message: ^msg] = any, 1_100
-	# end
+		assert_receive Mqttex.PublishMsg[message: ^msg] = any, 1_100
+	end
 
-	# test "Publish two ALOs" do
-	# 	{q, _qIn} = setupQueue()
-	# 	msg1 = "Initial Message ALO"
-	# 	Mqttex.Test.SessionAdapter.publish(q, "ALO-Topic", msg1, :at_least_once)
-	# 	assert_receive Mqttex.PublishMsg[message: ^msg1], 1_000
+	test "Publish two ALOs" do
+		{q, _qIn} = setupQueue()
+		msg1 = "Initial Message ALO"
+		Mqttex.Test.SessionAdapter.publish(q, "ALO-Topic", msg1, :at_least_once)
+		assert_receive Mqttex.PublishMsg[message: ^msg1], 1_000
 
-	# 	msg2 = "2nd Message ALO"
-	# 	Mqttex.Test.SessionAdapter.publish(q, "ALO-Topic", msg2, :at_least_once)
-	# 	assert_receive Mqttex.PublishMsg[message: ^msg2], 1_000
-	# end
+		msg2 = "2nd Message ALO"
+		Mqttex.Test.SessionAdapter.publish(q, "ALO-Topic", msg2, :at_least_once)
+		assert_receive Mqttex.PublishMsg[message: ^msg2], 1_000
+	end
 
 
-	# test "Many ALO messages" do
-	# 	{q, qIn} = setupQueue()
-	# 	messages = generateMessages(100)
-	# 	# IO.puts "messages are: #{inspect messages}"
+	test "Many ALO messages" do
+		{q, qIn} = setupQueue()
+		messages = generateMessages(100)
+		# IO.puts "messages are: #{inspect messages}"
 
-	# 	bulk_send(messages, q, :at_least_once, "ALO-Topic")
-	# 	result = slurp()
-	# 	IO.puts "Slurp result: #{inspect result}"
-	# 	Enum.each(messages, fn(m) -> assert result[m] > 0 end)
-	# end
+		bulk_send(messages, q, :at_least_once, "ALO-Topic")
+		result = slurp()
+		IO.puts "Slurp result: #{inspect result}"
+		Enum.each(messages, fn(m) -> assert result[m] > 0 end)
+	end
 
-	# test "Many AMO messages" do
-	# 	{q, qIn} = setupQueue()
-	# 	messages = generateMessages(100)
-	# 	# IO.puts "messages are: #{inspect messages}"
+	test "Many AMO messages" do
+		{q, qIn} = setupQueue()
+		messages = generateMessages(100)
+		# IO.puts "messages are: #{inspect messages}"
 
-	# 	bulk_send(messages, q, :at_most_once, "AMO-Topic")
-	# 	result = slurp()
-	# 	IO.puts "Slurp result: #{inspect result}"
-	# 	Enum.each(messages, fn(m) -> assert result[m] == 1 end)
-	# end
+		bulk_send(messages, q, :at_most_once, "AMO-Topic")
+		result = slurp()
+		IO.puts "Slurp result: #{inspect result}"
+		Enum.each(messages, fn(m) -> assert result[m] == 1 end)
+	end
 
-	# test "Many ALO messages via lossy queue" do
-	# 	{q, qIn} = setupQueue(50)
-	# 	messages = generateMessages(100)
-	# 	# IO.puts "messages are: #{inspect messages}"
+	test "Many ALO messages via lossy queue" do
+		{q, qIn} = setupQueue(50)
+		messages = generateMessages(100)
+		# IO.puts "messages are: #{inspect messages}"
 
-	# 	bulk_send(messages, q, :at_least_once, "ALO-Topic")
-	# 	result = slurp()
-	# 	IO.puts "Slurp result: #{inspect result}"
-	# 	Enum.each(messages, fn(m) -> assert result[m] > 0 end)
-	# end
+		bulk_send(messages, q, :at_least_once, "ALO-Topic")
+		result = slurp()
+		IO.puts "Slurp result: #{inspect result}"
+		Enum.each(messages, fn(m) -> assert result[m] > 0 end)
+	end
 
 	test "Many AMO messages via lossy queue" do
 		{q, qIn} = setupQueue(50)
