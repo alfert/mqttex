e1c1a2f715eb888a9742d7cd3d9c5febdad57516
Klaus Alfert <klaus.alfert@googlemail.com>
2014-03-15 00:40:22 +0100

    All test except delete are green again

--- test/mqttex_set_test.exs
+++ test/mqttex_set_test.exs
@@ -54,17 +54,70 @@ defmodule MqttexSetTest do
 			b = Mqttex.SubscriberSet.member?(s, e) 
 			assert there? == b,	"#{there?} == #{b}, for e = #{inspect e} in\n #{inspect s}"
 			end)
-		IO.puts "The print function"
+		end
+
+	test "Check the Enum Representation" do 
+		s0 = Mqttex.SubscriberSet.new()
+		es = [{false, {"/hello/world", {"my_client", :fire_and_forget}}},
+			{true, {"/hello/world/x", {"my_client", :fire_and_forget}}},
+			{true, {"/hello/world", {"my_client", :at_least_once}}},
+			{true, {"/hello/+", {"my_client", :fire_and_forget}}},
+			{true, {"/hello/#", {"my_client", :at_least_once}}}
+		]
+		# insert all subscriptions of es
+		s = Enum.reduce(es, s0, fn({_, e}, s) -> Mqttex.SubscriberSet.put(s, e) end)
+
+		# check that all subscriptions are there or not	
+		Enum.each(s, fn(sub)-> 
+			IO.puts "Check for #{inspect sub}"
+			assert Enum.member?(es, {true, sub}), "for sub = #{inspect sub} in\n #{inspect es}"
+			end)
+		IO.puts "\nThe print function"
 		Mqttex.SubscriberSet.print(s)
 		IO.puts "The reducer / Enum.each"
 		Enum.each(s, fn(e) -> IO.inspect(e) end)
 	end
 
-	#
-	# TO SUPPORT PROPER, A LIST REPRESENTATION WOULD BE HELPFUL
-	# ==> IMPLEMENT ENUMERABLE-INTERFACE?
-	# 
+	test "check partial equal function" do
+		s0 = Mqttex.SubscriberSet.new()
+		# es = [{unique_id, subscriber}] where unique_id is equal for "equal" subscriptions
+		es = [{1, {"/hello/world", {"my_client", :fire_and_forget}}}, 
+		 {2, {"/hello/world/x", {"my_client", :fire_and_forget}}},
+		 {1, {"/hello/world", {"my_client", :at_least_once}}},
+		 {3, {"/hello/+", {"my_client", :fire_and_forget}}},
+		 {4, {"/hello/#", {"my_client", :at_least_once}}}
+		]
 
+		# check eq function
+		Enum.each(es, fn({n, p}) -> 
+			Enum.each(es, fn({n1, p1}) -> 
+				case (n == n1) do
+					true -> assert eq_sub(p, p1)
+					false -> assert not eq_sub(p, p1)
+				end
+				end)
+			end)
+	end
+
+	test "delete function" do 
+		# create a set s of all kind of nodes from list l, but with some node having a common prefix
+		# test procedure:
+		#   delete an element from set s and from the list l
+		#   convert the set es to list ls and show that ls and l are equal if sorted
+		l = [{"/hello/world", {"my_client", :fire_and_forget}}, 
+		 {"/hello/world/x", {"my_client", :fire_and_forget}},
+		 {"/hello/world", {"my_client_2", :at_least_once}},
+		 {"/hello/+", {"my_client", :fire_and_forget}},
+		 {"/hello/#", {"my_client", :at_least_once}}
+		]
+		s0 = Mqttex.SubscriberSet.new()
+		# insert all subscriptions of es
+		s = Enum.reduce(l, s0, &(Mqttex.SubscriberSet.put(&2, &1)))
+		
+		ls = Enum.to_list(s)
+		assert Enum.sort(ls) == Enum.sort(l)
+		flunk "not implemented"
+	end
 
 	test "validity of topic paths" do
 		assert {true, _p} = Mqttex.SubscriberSet.convert_path("/xxx") 
@@ -82,4 +135,7 @@ defmodule MqttexSetTest do
 	end
 
 	defp clean(p), do: Enum.filter(p, fn(x) -> x != "/" end)
+
+	defp eq_sub({path, {node, _qos1}}, {path, {node, _qos2}}), do: true
+	defp eq_sub(x, y), do: false 
 end
