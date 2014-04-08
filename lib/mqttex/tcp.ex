ee99ba5b7bd314bbc11e4f7db777ece72d455410
Klaus Alfert <klaus.alfert@googlemail.com>
2014-04-08 18:38:55 +0200

    Handling of errors corrected and better log output

--- lib/mqttex/tcp.ex
+++ lib/mqttex/tcp.ex
@@ -21,15 +21,18 @@ defmodule Mqttex.TCP do
 											 {:reuseaddr, true},	
 											 {:active, true}])
 		Lager.info("Mqttex.Server at port #{port} is listening")
-		spawn(fn() -> spawn_server(listen) end)
+		spawn(fn() -> spawn_acceptor(listen) end)
 	end
 
-	defp spawn_server(listen) do
-		{:ok, socket} = :gen_tcp.accept(listen)
-		# :gen_tcp.close(listen)
-		Lager.info("Mqttex.Server accepted and spawning")
-		spawn(fn() -> spawn_server(listen) end)
-		loop(socket, :nil, Mqttex.Server)				
+	defp spawn_acceptor(listen) do
+		Lager.info("#{inspect self}: starting spawn_acceptor(#{inspect listen}")
+		case :gen_tcp.accept(listen) do
+			{:ok, socket} -> 
+				Lager.info("Mqttex.Server has accepted and spawns new acceptor")
+				spawn(fn() -> spawn_acceptor(listen) end)
+				loop(socket, :nil, Mqttex.Server)				
+			any -> Lager.info("accecpt returned #{inspect any} - finishing now")
+		end
 	end
 	
 	@doc """
@@ -61,9 +64,9 @@ defmodule Mqttex.TCP do
 		Lager.debug("loop #{inspect self} for #{inspect socket} and process #{inspect server} with module #{mod}")
 		receive do
 			{:tcp, ^socket, bin} ->
-				Lager.debug("Socket received binary = #{inspect bin}")
+				Lager.debug("loop #{inspect self}: Socket received binary = #{inspect bin}")
 				str = :erlang.binary_to_term(bin)
-				Lager.debug("Socket (unpacked) #{inspect str}")
+				Lager.debug("loop #{inspect self}: Socket (unpacked) #{inspect str}")
 				# call the server
 				case str do
 					Mqttex.ConnectionMsg[] = con -> 
@@ -73,9 +76,9 @@ defmodule Mqttex.TCP do
 						loop(socket, server, mod)
 				end
 			{:tcp_closed, ^socket} ->
-				Lager.info("Socket #{inspect socket} closed")
+				Lager.info("loop #{inspect self}: Socket #{inspect socket} closed")
 			msg ->
-				Lager.debug("Socket sends message #{inspect msg}")
+				Lager.info("loop #{inspect self}: Socket #{inspect socket} sends message #{inspect msg}")
 				:gen_tcp.send(socket, :erlang.term_to_binary(msg))
 				loop(socket, server, mod)
 		end
