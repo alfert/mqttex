8fabfaea8ff8dc861716a9f686f1edc8146424d9
Klaus Alfert <klaus.alfert@googlemail.com>
2014-05-25 14:30:27 +0200

    Decoding of length values

--- test/mqttex_decoder_test.exs
+++ test/mqttex_decoder_test.exs
@@ -10,25 +10,32 @@ defmodule MqttexQueueTest do
 	test "length one byte values" do
 		numbers = [0, 100, 127]
 		numbers |> Enum.each fn(n) -> 
-			assert n == Mqttex.Decoder.binary_to_length(<<n>>, 4, fn() -> nil end)
+			assert n == Mqttex.Decoder.binary_to_length(<<n>>, fn() -> nil end)
 		end
 	end
 
 	test "length 128 in one byte fails" do
-		catch_error Mqttex.Decoder.binary_to_length(<<128>>, 4, fn() -> nil end)
+		catch_error Mqttex.Decoder.binary_to_length(<<128>>, fn() -> nil end)
 	end
 
 	test "length with two bytes" do
 		bytes = %{ 0 => [0], 128 => [0x80, 0x01], 16_383 => [0xFF, 0x7F]}
 		bytes |> Enum.each fn({n, bs}) ->
-			assert n == Mqttex.Decoder.binary_to_length(<<hd bs>>, 4, fn () -> next_byte(tl bs) end)
+			assert n == Mqttex.Decoder.binary_to_length(<<hd bs>>, fn () -> next_byte(tl bs) end)
 		end 
 	end
 
 	test "length with three bytes" do
 		bytes = %{ 0 => [0], 16_384 => [0x80, 0x80, 0x01], 2_097_151 => [0xFF, 0xFF, 0x7F]}
 		bytes |> Enum.each fn({n, bs}) ->
-			assert n == Mqttex.Decoder.binary_to_length(<<hd bs>>, 4, fn () -> next_byte(tl bs) end)
+			assert n == Mqttex.Decoder.binary_to_length(<<hd bs>>, fn () -> next_byte(tl bs) end)
+		end 
+	end
+
+	test "length with four bytes" do
+		bytes = %{ 0 => [0], 2_097_152 => [0x80, 0x80, 0x80, 0x01], 268_435_455 => [0xFF, 0xFF, 0xFF, 0x7F]}
+		bytes |> Enum.each fn({n, bs}) ->
+			assert n == Mqttex.Decoder.binary_to_length(<<hd bs>>, fn () -> next_byte(tl bs) end)
 		end 
 	end
 
