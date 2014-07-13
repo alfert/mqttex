91266266e0a89b3d029fb1e058d0f5f2bb8aac15
Klaus Alfert <klaus.alfert@googlemail.com>
2014-07-13 12:37:42 +0200

    Waiting for published messages

--- test/mqttex_simpletcp_test.exs
+++ test/mqttex_simpletcp_test.exs
@@ -3,114 +3,114 @@ defmodule MqttexSimpleTCPTest do
 	import Mqttex.Test.Tools
 	require Lager
 
-	test "Environment is ok" do
-	    # env data for the application :mqttex is available
-	    assert {:ok, 1178} == :application.get_env(:mqttex, :port)
+	# test "Environment is ok" do
+	#     # env data for the application :mqttex is available
+	#     assert {:ok, 1178} == :application.get_env(:mqttex, :port)
 
-	    # Obviously, we are not in the application :mqttex
-	    assert :undefined == :application.get_env(:port) 
-	end
+	#     # Obviously, we are not in the application :mqttex
+	#     assert :undefined == :application.get_env(:port) 
+	# end
 
-	test "Basic TCP connections work" do
-		server = Mqttex.TCP.start_server
-		assert is_pid(server)
-		serverRef = Process.monitor server
+	# test "Basic TCP connections work" do
+	# 	server = Mqttex.TCP.start_server
+	# 	assert is_pid(server)
+	# 	serverRef = Process.monitor server
 
-		con = Mqttex.Client.new_connection({127, 0, 0, 1}, Mqttex.TCP)
-		{:ok, client} = Mqttex.Client.connect("any user", "passwd", self, con)
-		assert is_pid(client)
+	# 	con = Mqttex.Client.new_connection({127, 0, 0, 1}, Mqttex.TCP)
+	# 	{:ok, client} = Mqttex.Client.connect("any user", "passwd", self, con)
+	# 	assert is_pid(client)
 
-		# Waiting for ConnAck
-		# assert_receive(Mqttex.ConnAckMsg[status: :ok], 100, "Still no ConnAck :-(")
-		assert_receive(response, 1_000, "Still got no kind of ConnAck msg :-(")
-		assert %Mqttex.Msg.ConnAck{status: :ok} == response
+	# 	# Waiting for ConnAck
+	# 	# assert_receive(Mqttex.ConnAckMsg[status: :ok], 100, "Still no ConnAck :-(")
+	# 	assert_receive(response, 1_000, "Still got no kind of ConnAck msg :-(")
+	# 	assert %Mqttex.Msg.ConnAck{status: :ok} == response
 
-		# Publishing Hello
-		Mqttex.Client.publish(client, "topic", "Hello", :fire_and_forget)
+	# 	# Publishing Hello
+	# 	Mqttex.Client.publish(client, "topic", "Hello", :fire_and_forget)
 
-		# Disconnecting
-		Mqttex.Client.disconnect(client)
+	# 	# Disconnecting
+	# 	Mqttex.Client.disconnect(client)
 
-		# Server shall go down
-		wait_for_shutdown(serverRef)
-		refute Process.alive? server
+	# 	# Server shall go down
+	# 	wait_for_shutdown(serverRef)
+	# 	refute Process.alive? server
 
-		# Client shall be down
-		refute Process.alive? client
-	end
+	# 	# Client shall be down
+	# 	refute Process.alive? client
+	# end
 
-	test "Basic TCP re-connections work" do
-		server = Mqttex.TCP.start_server
-		assert is_pid(server)
-		serverRef = Process.monitor server
+	# test "Basic TCP re-connections work" do
+	# 	server = Mqttex.TCP.start_server
+	# 	assert is_pid(server)
+	# 	serverRef = Process.monitor server
 
-		conA = Mqttex.Client.new_connection({127, 0, 0, 1}, Mqttex.TCP)
-		{:ok, clientA} = Mqttex.Client.connect("any user", "passwd", self, conA)
-		assert is_pid(clientA)
-		clientARef = Process.monitor clientA
+	# 	conA = Mqttex.Client.new_connection({127, 0, 0, 1}, Mqttex.TCP)
+	# 	{:ok, clientA} = Mqttex.Client.connect("any user", "passwd", self, conA)
+	# 	assert is_pid(clientA)
+	# 	clientARef = Process.monitor clientA
 
-		# Waiting for ConnAck
-		assert_receive(%Mqttex.Msg.ConnAck{status: :ok}, 1_000, "Still no ConnAck :-(")
+	# 	# Waiting for ConnAck
+	# 	assert_receive(%Mqttex.Msg.ConnAck{status: :ok}, 1_000, "Still no ConnAck :-(")
 
-		# Disconnecting
-		Mqttex.Client.disconnect(clientA)
-		wait_for_shutdown(clientARef)
+	# 	# Disconnecting
+	# 	Mqttex.Client.disconnect(clientA)
+	# 	wait_for_shutdown(clientARef)
 
-		# Client shall be down
-		refute Process.alive? clientA
+	# 	# Client shall be down
+	# 	refute Process.alive? clientA
 
-		conB = Mqttex.Client.new_connection({127, 0, 0, 1}, Mqttex.TCP)
-		{:ok, clientB} = Mqttex.Client.connect("any user", "passwd", self, conB)
-		assert is_pid(clientB)
-		clientBRef = Process.monitor clientB
+	# 	conB = Mqttex.Client.new_connection({127, 0, 0, 1}, Mqttex.TCP)
+	# 	{:ok, clientB} = Mqttex.Client.connect("any user", "passwd", self, conB)
+	# 	assert is_pid(clientB)
+	# 	clientBRef = Process.monitor clientB
 
-		# Waiting for ConnAck
-		assert_receive(%Mqttex.Msg.ConnAck{status: :ok}, 1_000, "Still no ConnAck :-(")
+	# 	# Waiting for ConnAck
+	# 	assert_receive(%Mqttex.Msg.ConnAck{status: :ok}, 1_000, "Still no ConnAck :-(")
 
-		# Publishing Hello
-		Mqttex.Client.publish(clientB, "topic", "Hello", :fire_and_forget)
+	# 	# Publishing Hello
+	# 	Mqttex.Client.publish(clientB, "topic", "Hello", :fire_and_forget)
 
-		# Disconnecting
-		Mqttex.Client.disconnect(clientB)
-		wait_for_shutdown(clientBRef)
+	# 	# Disconnecting
+	# 	Mqttex.Client.disconnect(clientB)
+	# 	wait_for_shutdown(clientBRef)
 
-		# Server shall go down
-		wait_for_shutdown(serverRef)
-		refute Process.alive? server
+	# 	# Server shall go down
+	# 	wait_for_shutdown(serverRef)
+	# 	refute Process.alive? server
 
-		# Client shall be down
-		refute Process.alive? clientB
-	end
+	# 	# Client shall be down
+	# 	refute Process.alive? clientB
+	# end
 
 
-	test "Basic TCP connection, pings and disconnects work" do
-		server = Mqttex.TCP.start_server
-		assert is_pid(server)
-		serverRef = Process.monitor server
+	# test "Basic TCP connection, pings and disconnects work" do
+	# 	server = Mqttex.TCP.start_server
+	# 	assert is_pid(server)
+	# 	serverRef = Process.monitor server
 
-		con = Mqttex.Client.new_connection({127, 0, 0, 1}, Mqttex.TCP)
-		{:ok, client} = Mqttex.Client.connect("any user", "passwd", self, con)
-		assert is_pid(client)
+	# 	con = Mqttex.Client.new_connection({127, 0, 0, 1}, Mqttex.TCP)
+	# 	{:ok, client} = Mqttex.Client.connect("any user", "passwd", self, con)
+	# 	assert is_pid(client)
 
-		# Waiting for ConnAck
-		assert_receive(%Mqttex.Msg.ConnAck{status: :ok}, 1_000, "Still no ConnAck :-(")
+	# 	# Waiting for ConnAck
+	# 	assert_receive(%Mqttex.Msg.ConnAck{status: :ok}, 1_000, "Still no ConnAck :-(")
 
-		# Publishing Hello
-		Mqttex.Client.publish(client, "topic", "Hello", :fire_and_forget)
+	# 	# Publishing Hello
+	# 	Mqttex.Client.publish(client, "topic", "Hello", :fire_and_forget)
 
-		# Sleep sometime such that the ping mechanisms starts to work
-		sleep(2_000)
+	# 	# Sleep sometime such that the ping mechanisms starts to work
+	# 	sleep(2_000)
 
-		# Disconnecting
-		Mqttex.Client.disconnect(client)
+	# 	# Disconnecting
+	# 	Mqttex.Client.disconnect(client)
 
-		# Server shall go down
-		assert :ok == wait_for_shutdown(serverRef)
-		refute Process.alive? server
+	# 	# Server shall go down
+	# 	assert :ok == wait_for_shutdown(serverRef)
+	# 	refute Process.alive? server
 
-		# Client shall be down
-		refute Process.alive? client
-	end
+	# 	# Client shall be down
+	# 	refute Process.alive? client
+	# end
 
 	########################
 	#### Define an end2end Test with 
@@ -149,9 +149,12 @@ defmodule MqttexSimpleTCPTest do
 		:ok = Mqttex.Client.publish(pub, "topic", "Hello", :fire_and_forget)
 
 		# Waiting
-
+		receive do
+			message -> Lager.info "Got a message: #{inspect message} Is it a good one?"
+			after 1_000 -> flunk "got nothing from publishing"
+		end
 		# Sleep sometime such that the ping mechanisms starts to work
-		sleep(2_000)
+		# sleep(2_000)
 
 		# Disconnecting pub & sub
 		Mqttex.Client.disconnect(pub)
